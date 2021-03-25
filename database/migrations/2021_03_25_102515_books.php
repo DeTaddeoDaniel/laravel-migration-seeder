3dee02060ae89e37e5d1286df7aac6eaaaa57465
Daniel2711 <detaddeo.d@gmail.com>
2021-03-25 11:28:51 +0100

    Migrate inserito

---
+++ database/migrations/2021_03_25_102515_books.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Books extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('Books', function (Blueprint $table) {
+        $table->bigIncrements('id');
+        $table->string('slang');
+        $table->string('tipologia');
+        $table->text('descrizione');
+        $table->string('prodotto');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('flights');
+    }
+}
